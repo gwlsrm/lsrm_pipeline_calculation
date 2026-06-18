5bdca46a3586564d1dfc123d941afd8e24d353b2
gwqw <gwrass@mail.ru>
2026-06-18 20:10:30 +0300

    2pi-efficiency: add material input as json and sl-json, upd. readme

--- examples/2pi_efficiency/README.md
+++ examples/2pi_efficiency/README.md
@@ -2,25 +2,39 @@
 
 ## Description
 
-This pipeline is intended for the task: obtaining efficiency for 2pi geometry from efficiency for point geometry.
+This pipeline is intended for the task: obtaining special efficiency for 2pi geometry from efficiency for point geometry.
+The 2pi special efficiency 'eff' can be used for special activity calculation like:
+$$
+A_{special} = \frac {N} {eff * I * t}
+$$
+where `A_special` -- activity in Bq/kg, `N` -- peak area, `I` -- line intensity, `t` -- time, s
+
+Special efficiency is calculated by formula:
+$$
+\varepsilon_{special} = \frac {\varepsilon_0 r^2 2 \pi} {\mu(E)}
+$$
+where `epsilon_0` -- point efficiency, `r` -- point-detector distance, cm, `mu` -- mass att. coeff. for material, cm^2/g
+
 
 ## Install
 
-- copy this directory to projects
-- copy efa-file with point efficiency section
+- create "projects" directory in project root directory
+- copy this directory to "projects"
+- put efa-file with point efficiency section to "projects/2pi_efficiency"
 
 ## Run
-- edit graph `create_2pi_efficiency.yaml` parameters:
+- edit confug `create_2pi_efficiency.yaml` parameters:
     - "EfrFromEfaOperation" operation
         - put efa-file with point efficiency to `input_filename`,
         - put point efficiency section to `section_name`,
         - edit energy_grid,
     - "ExtendedObjectEfficiencyOperation" operation
         - put point distance from point efficiency to `distance`
-        - put material formula to `material`, e.g. H2O1 ("1" is crucial)
+        - put material formula to `material_formula`, e.g. H2O1 ("1" is crucial)
+        - or put material as json-string to `material_json` (look in example create_2pi_efficiency.yaml)
     - "EfrUpdateFromTsvOperation" operation
         - put desirable output filename to `output_filename`,
-- run efficiencies calculation:
+- run efficiencies calculation from terminal (command line):
 ```
 python run.py projects/2pi_efficiency/create_2pi_efficiency.yaml
 ```
--- operations/sl_extended_object_efficiency_operation.py
+++ operations/sl_extended_object_efficiency_operation.py
@@ -17,16 +17,15 @@ def _load_point_efficiency(input_filename: str) -> tuple[np.ndarray, np.ndarray,
     return np.array(res['energy']), np.array(res['efficiency']), np.array(res['defficiency'])
 
 
-def _load_mu(material_formula: str, energies: np.ndarray) -> np.ndarray:
+def _load_mu(material: Material, energies: np.ndarray) -> np.ndarray:
     mu_db = MuDB.read_from_directory("XCOM")
-    material = Material.parse_from_formula(material_formula)
     return np.array(get_material_mus(material, mu_db, energies / keV2MeV))
 
 
 def calculate_extended_object_efficiency(
-        energies: np.ndarray, point_efficiency: np.ndarray, distance: float, material_formula: str
+        energies: np.ndarray, point_efficiency: np.ndarray, distance: float, material: Material
     ) -> np.ndarray:
-    mu = _load_mu(material_formula, energies)
+    mu = _load_mu(material, energies)
     return point_efficiency * distance**2 * 2 * np.pi / mu * g2kg
 
 
@@ -51,13 +50,19 @@ class ExtendedObjectEfficiencyOperation:
     params:
         input_filename: str -- point efficiency tsv-file
         distance: float -- distance of point efficiency, cm
-        material: str -- material formula like H2O1
+        material_formula: str -- material formula like H2O1
+        material_json: str -- material in json-format: {name: ..., rho: ..., elements: [{z:..., frac: ...}, ...]}
+            e.g. {"name": "water", "rho": 1.0, "elements": [{"z": 1, "frac": 0.11}, {"z": 8, "frac": 0.89}]}
+        sl_material_json: str -- material in json-format: {Name: ..., Ro: ..., Compound: [{"z": frac}, ...]}
+            e.g. {"Name": "water", "Ro": 1.0, "Compound": [{"1": 0.11}, {"8": 0.89}]}
         output_filename: str -- specific efficiency tsv-file in kg/(Bq * s)
     """
     def __init__(self):
         self.input_filename = ""
         self.distance = 0.0
-        self.material = ""
+        self.material_formula = ""
+        self.material_json = ""
+        self.sl_material_json = ""
         self.output_filename = ""
 
     @staticmethod
@@ -65,12 +70,22 @@ class ExtendedObjectEfficiencyOperation:
         op = ExtendedObjectEfficiencyOperation()
         op.input_filename = os.path.join(project_dir, section['input_filename'])
         op.distance = float(section['distance'])
-        op.material = section['material']
+        op.material_formula = section.get('material_formula', "")
+        op.material_json = section.get('material_json', "")
+        op.sl_material_json = section.get('sl_material_json', "")
+        assert op.material_json or op.material_formula or op.sl_material_json, \
+            "need to pass material_formula, material_json or sl_material_json"
         op.output_filename = os.path.join(project_dir, section['output_filename'])
         return op
 
     def run(self) -> None:
         print('start extended object efficiency operation')
         energies, point_efficiency, defficiency = _load_point_efficiency(self.input_filename)
-        ext_efficiency = calculate_extended_object_efficiency(energies, point_efficiency, self.distance, self.material)
+        if self.material_formula:
+            material = Material.parse_from_formula(self.material_formula)
+        elif self.material_json:
+            material = Material.read_from_json(self.material_json)
+        else:
+            material = Material.read_from_sl_json(self.sl_material_json)
+        ext_efficiency = calculate_extended_object_efficiency(energies, point_efficiency, self.distance, material)
         _save_to_tsv(energies, ext_efficiency, defficiency, self.output_filename)
